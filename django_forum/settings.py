db5b77081774cba1dc24b782c68e6008c05c0c42
Ivan Gonzalez <cesargonzalez96@protonmail.com>
2022-04-10 18:52:34 -0700

    added allowed host

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-8!xez%8m)ju#5^a1z1(15^j4vy&$)+h*@t%n6)ux!%w$0o_pdy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://git.heroku.com/boiling-lake-90284.git', '127.0.0.8000']
 
 
 # Application definition
